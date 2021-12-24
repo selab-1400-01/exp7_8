15e84e527e4d12eaad4a02bed39aecdaca0cac38
momt99 <mohammad.omt99@yahoo.com>
2021-12-24 12:23:16 +0330

    Add swagger for patients

--- projects/patient/patient/urls.py
+++ projects/patient/patient/urls.py
@@ -1,13 +1,28 @@
-from django.urls import include, path
-from rest_framework import routers
+from django.urls import include, path, re_path
+from rest_framework import routers, permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from main import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.PatientViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Patients",
+      default_version="v1",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
