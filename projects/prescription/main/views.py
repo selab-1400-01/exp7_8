7d43264b933a3a428e24b4ad989f098749d6f505
momt99 <mohammad.omt99@yahoo.com>
2021-12-25 01:47:54 +0330

    Add prescription list based on token

--- projects/prescription/main/views.py
+++ projects/prescription/main/views.py
@@ -1,4 +1,5 @@
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from main.models import Prescription
 from main.serializers import PrescriptionSerializer
@@ -6,19 +7,21 @@ from rest_framework import generics
 from rest_framework.views import APIView
 
 
-class PrescriptionList(generics.ListCreateAPIView):
+class PrescriptionList(generics.ListAPIView):
     queryset = Prescription.objects.all()
     serializer_class = PrescriptionSerializer
+    permission_classes = [IsAuthenticated]
 
+    def get(self, request, *args, **kwargs):
+        if "doctors" in request.user.groups:
+            queryset = self.get_queryset().filter(doctor__national_id=request.user.national_id)
+        else:
+            queryset = self.get_queryset().filter(patient__national_id=request.user.national_id)
 
-class PrescriptionDetail(generics.RetrieveUpdateDestroyAPIView):
-    lookup_field = 'patient_national_id'
-    queryset = Prescription.objects.all()
-    serializer_class = PrescriptionSerializer
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
 
-    def retrieve(self, request, *args, **kwargs):
-        pnid = self.kwargs.get('patient_national_id')
-        dnid = self.kwargs.get('doctor_national_id')
-        object = Prescription.objects.get(patient_national_id=pnid, doctor_national_id=dnid)
-        serializer = PrescriptionSerializer(object)
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
