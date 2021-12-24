37b3fde9647a8ca232d20826abb1b94172747602
Mahdi Erfanian <mahdi.erfanian@divar.ir>
2021-12-24 12:44:35 +0330

    :recycle: Refactor projects

--- projects/doctor/main/views.py
+++ projects/doctor/main/views.py
@@ -1,15 +1,3 @@
-from django.contrib.auth.models import User
-from rest_framework import viewsets
-
-from main.serializers import DoctorSerializer
-
-# class DoctorViewSet(viewsets.ModelViewSet):
-#     """
-#     API endpoint that allows users to be viewed or edited.
-#     """
-#     queryset = User.objects.all().order_by('-date_joined')
-#     serializer_class = DoctorSerializer
-
 from main.models import Doctor
 from main.serializers import DoctorSerializer
 from rest_framework import generics
@@ -18,7 +6,6 @@ from rest_framework import generics
 class DoctorList(generics.ListCreateAPIView):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
-
 
 
 class DoctorDetail(generics.RetrieveUpdateDestroyAPIView):
