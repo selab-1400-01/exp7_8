e2848c61b48687202f8efe10c549ae866c81dcd0
Mahdi Erfanian <mahdi.erfanian@divar.ir>
2021-12-24 12:37:50 +0330

    Fix patient api

--- projects/patient/main/views.py
+++ projects/patient/main/views.py
@@ -1,12 +1,15 @@
-from django.contrib.auth.models import User
-from rest_framework import viewsets
-
+from main.models import Patient
 from main.serializers import PatientSerializer
+from rest_framework import generics
 
 
-class PatientViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
+class PatientList(generics.ListCreateAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+
+
+class PatientDetail(generics.RetrieveUpdateDestroyAPIView):
+    lookup_field = 'national_id'
+    queryset = Patient.objects.all()
     serializer_class = PatientSerializer
